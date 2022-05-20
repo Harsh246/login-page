ca283b37d6194b932560f92d9525c19356ba5e49
Harsh <harsh0564.cse19@chitkara.edu.in>
2022-05-20 15:05:14 +0530

    template done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { LoginBoxComponent } from './login-box/login-box.component';
+import { RegisterBoxComponent } from './register-box/register-box.component';
+ 
+import {LoginService} from './login.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    LoginBoxComponent,
+    RegisterBoxComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
